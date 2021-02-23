984b8cf648e62351e9a69507b7f1c8d157faf194
Ananya31-tkm <74641663+Ananya31-tkm@users.noreply.github.com>
2021-02-23 21:46:43 +0530

    CO4

--- CO2-q11.py
+++ CO2-q11.py
@@ -1,8 +1,9 @@
+S=int(input("ENTER SIDE:"))
 l=int(input("ENTER LENGTH:"))
 b=int(input("ENTER BREADTH:"))
 h=int(input("ENTER HEIGHT:"))
 x = lambda a : a * a
-print("AREA OF SQUARE IS ",(x(l)))
+print("AREA OF SQUARE IS ",(x(S)))
 y = lambda a, b : a * b
 print("AREA OF RECTANGLE IS ",(y(l, b)))
 z = lambda a, b : 1/2 * (a * b)
